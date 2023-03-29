c7d3c1bc53742e0b64e539a60d2d396b8130e7e6
Evgeny Alekseenko <punconge@gmail.com>
2023-03-30 08:13:21 +1000

    test screen setup

--- lib/ui/screen/test_screen.dart
+++ lib/ui/screen/test_screen.dart
@@ -1,145 +1,281 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:places/data/interactor/place_interactor.dart';
-import 'package:places/data/model/place.dart';
-import 'package:places/data/repository/place_repository.dart';
-import 'package:places/mocks.dart';
+import 'package:places/app_router.dart';
 import 'package:places/ui/providers/filter_provider.dart';
+import 'package:places/ui/providers/search_provider.dart';
 import 'package:places/ui/providers/sight_list_provider.dart';
+import 'package:places/ui/ui_kit/ui_kit.dart';
 import 'package:places/ui/widget/bottom_nav_bar.dart';
-import 'package:places/ui/widget/colored_button.dart';
-import 'package:places/ui/widget/sight_card_tab.dart';
+import 'package:places/ui/widget/card_list.dart';
+import 'package:places/ui/widget/gradient_button.dart';
+import 'package:places/ui/widget/nothing_found.dart';
+import 'package:places/ui/widget/progress_indicator.dart';
+import 'package:places/ui/widget/search_bar.dart';
+import 'package:places/ui/widget/sight_card.dart';
 import 'package:provider/provider.dart';
 
-class TestScreen extends StatelessWidget {
+class TestScreen extends StatefulWidget {
   const TestScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    dynamic data;
+  State<TestScreen> createState() => _TestScreenState();
+}
 
-    return Scaffold(
-      bottomNavigationBar: const BottomNavBar(index: 4),
-      /*  body: Center(
-        child: ColoredButton(
-          text: 'DIO TEST',
-          onPressed: () async {
-            //data = await getPosts();
-            data = await PlaceRepository().getPlaces();
-            // ignore: avoid_print, avoid_dynamic_calls
-            print(data[0].urls);
-          },
+class _TestScreenState extends State<TestScreen> {
+  @override
+  void initState() {
+    context.read<SightListProvider>().isloading = true;
+    context
+        .read<SightListProvider>()
+        .loadFilteredPlaces(context.read<FilterProvider>().filter);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Consumer<SightListProvider>(builder: (context, provider, child) {
+      final orientation = MediaQuery.of(context).orientation;
+
+      return Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            if (orientation == Orientation.portrait)
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _SliverTitleDelegate(
+                  systemBarHeight: MediaQuery.of(context).padding.top,
+                  bigTitleStyle: theme.primaryTextTheme.headline1!,
+                  smallTitleStyle: theme.primaryTextTheme.headline3!,
+                  context: context,
+                ),
+              )
+            else
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: _LandscapeSliverTitleDelegate(
+                  systemBarHeight: MediaQuery.of(context).padding.top,
+                  bigTitleStyle: theme.primaryTextTheme.headline1!,
+                  smallTitleStyle: theme.primaryTextTheme.headline3!,
+                  context: context,
+                ),
+              ),
+            if (context.watch<SightListProvider>().isloading)
+              const SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: CustomProgress(),
+                ),
+              )
+            else if (provider.placeList.isEmpty)
+              const SliverToBoxAdapter(
+                child: NotFound(),
+              )
+            else
+              SliverCardList(
+                iterable: provider.placeList,
+                type: CardType.list,
+              ),
+          ],
         ),
-      ), */
-      appBar: AppBar(
-        actions: [
-          InkWell(
-            onTap: () {
-              PlaceInteractor()
-                  .getFilteresPlaces(context.read<FilterProvider>().filter);
-            },
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.blue,
-              child: const Text('filter'),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GradientButton(
+              text: 'ОБНОВИТЬ',
+              onPressed: () {
+                ///
+                ///
+                /// обновлять лист
+                ///
+                ///
+              },
+              icon: AppIcons.add(
+                height: 18,
+                width: 18,
+                color: AppColors.primaryLightFFF,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: const BottomNavBar(index: 0),
+      );
+    });
+  }
+}
+
+class _SliverTitleDelegate extends SliverPersistentHeaderDelegate {
+  static const bigTitleOffset = 40;
+  final double systemBarHeight;
+  final TextStyle bigTitleStyle;
+  final TextStyle smallTitleStyle;
+  final double bigTitleHeight;
+  final double smallTitleHeight;
+  final BuildContext context;
+
+  @override
+  double get maxExtent =>
+      systemBarHeight + bigTitleOffset + bigTitleHeight * 2 + 30 + 48 + 16;
+
+  @override
+  double get minExtent => systemBarHeight + smallTitleHeight + 30;
+
+  _SliverTitleDelegate({
+    required this.systemBarHeight,
+    required this.bigTitleStyle,
+    required this.smallTitleStyle,
+    required this.context,
+  })  : bigTitleHeight = bigTitleStyle.fontSize! * bigTitleStyle.height!,
+        smallTitleHeight = smallTitleStyle.fontSize! * smallTitleStyle.height!;
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final flexibleSpace = bigTitleOffset + bigTitleHeight;
+    final k = shrinkOffset >= flexibleSpace
+        ? 0.0
+        : (flexibleSpace - shrinkOffset) / flexibleSpace;
+    final theme = Theme.of(context);
+
+    return Container(
+      color: theme.backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: systemBarHeight + k * bigTitleOffset + 16,
+          ),
+          Align(
+            alignment: Alignment(lerpDouble(0, -1, k)!, 0),
+            child: SizedBox(
+              height: smallTitleHeight +
+                  (bigTitleHeight * 2 - smallTitleHeight) * k,
+              child: Text(
+                shrinkOffset > 0
+                    ? 'Список интересных мест'
+                    : 'Список\nинтересных мест',
+                style: TextStyle.lerp(smallTitleStyle, bigTitleStyle, k),
+              ),
             ),
           ),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.amber,
-              child: const Text('post'),
+          if (shrinkOffset > 88)
+            const SizedBox.shrink()
+          else
+            Column(
+              children: [
+                const SizedBox(height: 30),
+                SearchBar(
+                  onPressed: () {
+                    context.read<SearchProvider>().newSightList(
+                          context.read<SightListProvider>().placeList,
+                        );
+
+                    Navigator.of(context).pushNamed(
+                      AppRouter.searchScreen,
+                    );
+                  },
+                  filters: context.watch<FilterProvider>().isEmpty,
+                  onSuffixPressed: () async {
+                    await Navigator.of(context).pushNamed(
+                      AppRouter.filterScreen,
+                    );
+                  },
+                  onSubmit: (_) {},
+                ),
+              ],
             ),
-          ),
-          InkWell(
-            onTap: () {
-              //context.read<SightListProvider>().loadPlaces();
-              PlaceInteractor().getPlaces();
-            },
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
-              child: const Text('get'),
-            ),
-          ),
         ],
-      ),
-      body: ListView.builder(
-        itemCount: context.read<SightListProvider>().placeList.length,
-        itemBuilder: (context, index) {
-          return context
-                  .read<SightListProvider>()
-                  .placeList[index]
-                  .id
-                  .toString()
-                  .contains('8887')
-              ? ListTile(
-                  title: Text(
-                    '${context.read<SightListProvider>().placeList[index].name} : ${context.read<SightListProvider>().placeList[index].id}',
-                  ),
-                )
-              : const SizedBox.shrink();
-        },
       ),
     );
   }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
 }
 
-//place
-///
-///
-///
+class _LandscapeSliverTitleDelegate extends SliverPersistentHeaderDelegate {
+  static const bigTitleOffset = 40;
+  final double systemBarHeight;
+  final TextStyle bigTitleStyle;
+  final TextStyle smallTitleStyle;
+  final double bigTitleHeight;
+  final double smallTitleHeight;
+  final BuildContext context;
 
-const testUrl = 'https://jsonplaceholder.typicode.com';
+  @override
+  double get maxExtent =>
+      systemBarHeight + smallTitleHeight + 30 + 25 + 40 + 14;
 
-final dio = Dio(baseoptions);
+  @override
+  double get minExtent =>
+      systemBarHeight + smallTitleHeight + 30 + 25 + 40 + 14;
 
-BaseOptions baseoptions = BaseOptions(
-  baseUrl: testUrl,
-  connectTimeout: 5000,
-  receiveTimeout: 5000,
-  sendTimeout: 5000,
-  // ignore: avoid_redundant_argument_values
-  responseType: ResponseType.json,
-);
+  _LandscapeSliverTitleDelegate({
+    required this.systemBarHeight,
+    required this.bigTitleStyle,
+    required this.smallTitleStyle,
+    required this.context,
+  })  : bigTitleHeight = bigTitleStyle.fontSize! * bigTitleStyle.height!,
+        smallTitleHeight = smallTitleStyle.fontSize! * smallTitleStyle.height!;
 
-Future<dynamic> getPosts() async {
-  initInterceptors();
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    final theme = Theme.of(context);
 
-  final response = await dio.get<String>('/users');
-  if (response.statusCode == 200) {
-    return response.data;
+    return Container(
+      color: theme.backgroundColor,
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Column(
+        children: [
+          SizedBox(
+            height: systemBarHeight + 16,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              ' Список интересных мест',
+              style: smallTitleStyle,
+            ),
+          ),
+          Column(
+            children: [
+              const SizedBox(height: 30),
+              SearchBar(
+                onPressed: () {
+                  context.read<SearchProvider>().newSightList(
+                        context.read<SightListProvider>().placeList,
+                      );
+
+                  Navigator.of(context).pushNamed(
+                    AppRouter.searchScreen,
+                  );
+                },
+                filters: context.watch<FilterProvider>().isEmpty,
+                onSuffixPressed: () async {
+                  await Navigator.of(context).pushNamed(
+                    AppRouter.filterScreen,
+                  );
+                },
+                onSubmit: (_) {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
-  throw Exception('No 200 status code: Error code: ${response.statusCode}');
-}
 
-void initInterceptors() {
-  dio.interceptors.add(
-    InterceptorsWrapper(
-      onError: (e, handler) {
-        // ignore: avoid_print
-        print(e.message);
-
-        return handler.next(e);
-      },
-      onRequest: (options, handler) {
-        // ignore: avoid_print
-        print('Запрос отправляется');
-
-        return handler.next(options);
-      },
-      onResponse: (e, handler) {
-        // ignore: avoid_print
-        print('Ответ получен');
-
-        return handler.next(e);
-      },
-    ),
-  );
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
 }
