ccbf62ebe4cb5e8993cfe3f9713558c11ef7867b
Evgeny Alekseenko <punconge@gmail.com>
2023-04-18 13:13:55 +1000

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:places/app.dart';
+import 'package:places/app_bloc_observer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  Bloc.observer = AppBlocObserver();
+
   runApp(const App());
 }
